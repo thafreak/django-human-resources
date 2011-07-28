61b20944e53c241504fa974be3e2e8883d24e8ec
Ryan Archdeacon <ryan@archweblabs.com>
2011-07-28 09:16:47 -0700

    Removing stale files

--- hr/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from hr.models import Evaluation
-
-class EvaluationAddForm(forms.ModelForm):
-	class Meta:
-		model = Evaluation
-		exclude = ('satisfied_qualifications',)
