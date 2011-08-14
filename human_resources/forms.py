e4c8a9ab83162aa95435080a53588269bcc5083e
Ryan Archdeacon <ryan@archweblabs.com>
2011-08-14 00:57:40 -0700

    Reforctoring

--- human_resources/forms.py
+++ human_resources/forms.py
@@ -20,25 +20,9 @@ class EvaluationChangeForm(forms.ModelForm):
 		
 		job_opportunity = self.instance.candidacy.job_opportunity
 		job_opportunity_position = job_opportunity.position
-		relevant_roles = job_opportunity_position.responsibilities.all()
 		
-		
-		# calcualte the ids for the relevant roles for use in querying
-		# qualifications and nice to haves
-		relevant_roles_ids = []
-		qualifications_queryset = None
-		nice_to_haves_queryset = None
-		
-		if relevant_roles:
-			for role in relevant_roles:
-				relevant_roles_ids.append(role.id)
-				
-			qualifications_queryset = Qualification.objects.filter(role__pk__in=relevant_roles_ids)
-			nice_to_haves_queryset = NiceToHave.objects.filter(role__pk__in=relevant_roles_ids)
-			
-		else: 
-			qualifications_queryset = Qualification.objects.empty()
-			nice_to_haves_queryset = NiceToHave.objects.empty()
+		qualifications_queryset = Qualification.objects.filter(position__pk=self.instance.id)
+		nice_to_haves_queryset = NiceToHave.objects.filter(position__pk=self.instance.id)
 		
 		self.fields['satisfied_qualifications'].queryset = qualifications_queryset
 		self.fields['satisfied_nice_to_haves'].queryset = nice_to_haves_queryset
